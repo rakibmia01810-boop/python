e50ce16051af1689486ded10300528b7053fe8a0
rakibmia01810-boop <rakibmia01810@gmail.com>
2025-11-06 13:24:35 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Main entry point for the Telegram bot.
+This file is used for deployment platforms that expect main.py
+"""
+
+from bot import main
+
+if __name__ == "__main__":
+    main()
+
